af98b0bbe78948f92fc6f393c16056eed46ed665
Steve Medrano <60121429+darknight101-pixel@users.noreply.github.com>
2020-07-28 21:36:59 -0500

    basic javascript

---
+++ home.js
@@ -0,0 +1,30 @@
+console.log("hello");
+alert("steve");
+
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById("someText").innerHTML = "different from react";
+
+////////////Showing variable/////////
+
+// var question = prompt("What do you like React over Angular");
+// document.getElementById("someText").innerHTML = question;
+
+/////////Showing variable//////////
+
+//Numbers///
+var num1 = 10;
+num1 = num1 + 1;
+
+//increment
+num1++;
+//Decrement
+num1--;
+
+//increment/ decrement by any number
+num1 += 10;
+console.log(num1);
